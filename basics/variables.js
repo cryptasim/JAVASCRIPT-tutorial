04101bd744cb65291bb23e39c632e74365730d47
cryptasim <128727510+cryptasim@users.noreply.github.com>
2024-01-17 16:28:51 +0000

    container config

---
+++ basics/variables.js
@@ -0,0 +1,16 @@
+const accountId = 1136
+let accountEmail = "asimkumarhansda@gmail.com"
+var accountPassword = "this password is weak"
+/*
+Prefer not to use var 
+because issue in block scope and function scope
+*/
+// not preferred
+accountCity = "Kharagpur"
+// accountId = 1 // not allowed
+accountEmail = "asimkrhansda@outlook.com"
+accountPassword = "this password was changed"
+accountCity = "new york"
+let accountState;
+console.log(accountId); 
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
