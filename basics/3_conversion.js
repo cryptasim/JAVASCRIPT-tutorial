b41d551961dd032951eeee2486b2dedf57b2830f
cryptasim <128727510+cryptasim@users.noreply.github.com>
2024-01-17 18:42:25 +0000

    3_conversion.js

--- basics/3_conversion.js
+++ basics/3_conversion.js
@@ -16,3 +16,14 @@ let isLoggedin = 1;
 let isBoolean = Boolean(isLoggedin);
 console.log(typeof isBoolean); //boolean
 console.log(isBoolean); //true
+
+/*
+1 => true; 0 => false 
+" " => false 
+"Asim" => true
+*/ 
+
+let someNumber = 1136
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber); //string
+console.log(stringNumber); //1136  
