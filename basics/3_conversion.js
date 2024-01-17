f6d818a5b3bffb0ecf3f6bbd3336a80a3a0103cd
cryptasim <128727510+cryptasim@users.noreply.github.com>
2024-01-17 18:12:41 +0000

    container config

---
+++ basics/3_conversion.js
@@ -0,0 +1,6 @@
+let score = 33
+console.log(typeof score) //number
+let value = "33"
+console.log(typeof value) //string 
+let nums = Number(value)
+console.log(typeof nums) //number 
--- conversion.js
+++
@@ -1,6 +0,0 @@
-let score = 33
-console.log(typeof score)
-let value = "33"
-console.log(typeof value)
-let nums = Number(value)
-console.log(typeof nums)
