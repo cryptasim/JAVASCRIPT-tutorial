a659d40004bcd6bdc7ed0621c1b870c4d0507516
cryptasim <128727510+cryptasim@users.noreply.github.com>
2024-01-17 17:00:23 +0000

    datatypes

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -3,9 +3,9 @@
 //tc39
 //mdn
 
-let age = 20
-let nam = "Asim"
-let supple = false
+let age = 20;
+let nam = "Asim";
+let supple = false;
 
 /*
 number    =>  2^53
@@ -16,8 +16,8 @@ null      => standalone value
 undefined => value not assigned
 symbol => unique 
 */
-console.log(typeof age);//number 
+console.log(typeof age); //number
 console.log(typeof nam); //string
-console.log(typeof supple);//boolean
-console.log(typeof null) //object
-console.log(typeof undefined) //undefined
+console.log(typeof supple); //boolean
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
