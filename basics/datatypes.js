9b3ae07c49f697c9947b43048ee6cf2f2a859864
cryptasim <128727510+cryptasim@users.noreply.github.com>
2024-01-17 16:59:24 +0000

    container config push

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -3,6 +3,10 @@
 //tc39
 //mdn
 
+let age = 20
+let nam = "Asim"
+let supple = false
+
 /*
 number    =>  2^53
 bigint    => more than number 
@@ -10,4 +14,10 @@ boolean   => true/false
 string    => " "
 null      => standalone value
 undefined => value not assigned
+symbol => unique 
 */
+console.log(typeof age);//number 
+console.log(typeof nam); //string
+console.log(typeof supple);//boolean
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
