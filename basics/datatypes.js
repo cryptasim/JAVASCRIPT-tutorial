0a52b6da868232e47b194dfe0be9c83e7fe1bb4e
cryptasim <128727510+cryptasim@users.noreply.github.com>
2024-01-17 16:52:28 +0000

    container

---
+++ basics/datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all js code as new version
+// alert("hello") we are using node js not browser
+//tc39
+//mdn
+
+/*
+number    =>  2^53
+bigint    => more than number 
+boolean   => true/false 
+string    => " "
+null      => standalone value
+undefined => value not assigned
+*/
