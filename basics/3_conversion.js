4c0df58e7aee00b4ff5acc16612d6652ca044071
cryptasim <128727510+cryptasim@users.noreply.github.com>
2024-01-17 18:41:50 +0000

    container config

--- basics/3_conversion.js
+++ basics/3_conversion.js
@@ -1,6 +1,18 @@
-let score = 33
-console.log(typeof score) //number
-let value = "33"
-console.log(typeof value) //string 
-let nums = Number(value)
-console.log(typeof nums) //number 
+let score = 33;
+console.log(typeof score); //number
+let value = "33";
+console.log(typeof value); //string
+let nums = Number(value);
+console.log(typeof nums); //number
+console.log(nums); //33
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1 false => 0
+*/
+ 
+let isLoggedin = 1;
+let isBoolean = Boolean(isLoggedin);
+console.log(typeof isBoolean); //boolean
+console.log(isBoolean); //true
