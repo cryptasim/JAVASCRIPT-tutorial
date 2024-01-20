b767e6d259e773765cb67dd97979a323b86614eb
cryptasim <128727510+cryptasim@users.noreply.github.com>
2024-01-20 06:21:21 +0000

    02_basics

---
+++ 02_basics/2_arrays.js
@@ -0,0 +1,20 @@
+// const marvel = ["IRONMAN", "SPIDERMAN", "CAPTAIN AMERICA"]
+// const dc = ["SUPERMAN","BATMAN","FLASH"]
+// // marvel.push(dc)
+// // console.log(marvel); //['IRONMAN','SPIDERMAN','CAPTAIN AMERICA',[ 'SUPERMAN', 'BATMAN', 'FLASH' ]]
+// // marvel.concat(dc)
+// // console.log(marvel); //['IRONMAN','SPIDERMAN','CAPTAIN AMERICA',[ 'SUPERMAN', 'BATMAN','FLASH' ]]
+
+// const heros = marvel.concat(dc)
+// console.log(heros); //['IRONMAN','SPIDERMAN','CAPTAIN AMERICA','SUPERMAN','BATMAN''FLASH']
+
+// const newheros = [...marvel, ...dc]
+// console.log(newheros); //['IRONMAN','SPIDERMAN','CAPTAIN AMERICA','SUPERMAN','BATMAN','FLASH']
+
+
+const arrarr = new Array(1,2,3,[4,5,6],[7,8,[9,10]])
+console.log(arrarr);
+
+ const okarr = arrarr.flat(Infinity)
+ console.log(okarr);
+
